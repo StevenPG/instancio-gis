4c18d89a37ff5b9b5af8fde96705de24b2050e65
Steven Gantz <steven.p.gantz@gmail.com>
2025-12-20 19:50:46 -0500

    Add additional tests in the example project

---
+++ example/src/test/java/com/stevenpg/app/GeometryGeneratorTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2025 Steven Gantz.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stevenpg.app;
+
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.Geometry;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeometryGeneratorTest {
+
+    @Test
+    void shouldGenerateGeometryUsingInstancio() {
+        var geom = Instancio.create(Geometry.class);
+        assertNotNull(geom);
+    }
+
+    @Test
+    void shouldGenerateGeometryWithinEnvelopeUsingGenerator() {
+        var bounds = new Envelope(-5, 5, -5, 5);
+        var geom = new com.stevenpg.instancio.locationtech.core.internal.generator.geom.GeometryGenerator()
+                .within(bounds)
+                .generate(null);
+
+        assertNotNull(geom);
+        var env = geom.getEnvelopeInternal();
+        assertTrue(env.getMinX() >= bounds.getMinX());
+        assertTrue(env.getMaxX() <= bounds.getMaxX());
+        assertTrue(env.getMinY() >= bounds.getMinY());
+        assertTrue(env.getMaxY() <= bounds.getMaxY());
+    }
+}
---
+++ example/src/test/java/com/stevenpg/app/LineStringGeneratorTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 Steven Gantz.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stevenpg.app;
+
+import com.stevenpg.instancio.locationtech.core.GenLocationtechJtsCore;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.LineString;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineStringGeneratorTest {
+
+    @Test
+    void shouldGenerateLineStringUsingInstancio() {
+        var ls = Instancio.create(LineString.class);
+        assertNotNull(ls);
+        assertTrue(ls.getNumPoints() >= 2);
+    }
+
+    @Test
+    void shouldGenerateLineStringUsingGenerator() {
+        var ls = GenLocationtechJtsCore.lineString()
+                .length(3)
+                .generate(null);
+        assertEquals(3, ls.getNumPoints());
+    }
+}
