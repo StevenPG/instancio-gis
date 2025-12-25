4f366e08318fc702ad5ec5882c8dae248e1c18ab
Steven Gantz <steven.p.gantz@gmail.com>
2025-12-25 04:23:25 -0500

    Finalize release process with 0.0.1 added to maven and github

--- buildSrc/src/main/kotlin/jreleaser-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/jreleaser-conventions.gradle.kts
@@ -5,35 +5,5 @@ plugins {
 }
 
 jreleaser {
-    project {
-        name.set("instancio-gis")
-        version.set("0.0.1")
-        description.set("Instancio support for GIS types")
-        authors.set(listOf("StevenPG"))
-        license.set("Apache-2.0")
-        links {
-            homepage.set("https://stevenpg.com/instancio-gis")
-        }
-        inceptionYear.set("2025")
-    }
-    signing {
-        active.set(Active.RELEASE)
-        armored.set(true)
-    }
-
-    deploy {
-        maven {
-            mavenCentral {
-                register("sonatype") {
-                    active.set(Active.RELEASE)
-                    url.set("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
-                    sign.set(true)
-                    stagingRepository("staging/")
-                    sourceJar.set(true)
-                    checksums.set(true)
-                    javadocJar.set(true)
-                }
-            }
-        }
-    }
+    configFile.set(layout.projectDirectory.file("config.yaml"))
 }
